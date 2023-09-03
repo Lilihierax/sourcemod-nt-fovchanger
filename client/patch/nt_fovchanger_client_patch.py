76c757c505109ddad08bece669fac3bc4b3c5875
Lilihierax <140167708+Lilihierax@users.noreply.github.com>
2023-09-03 19:25:08 +0300

    Update nt_fovchanger_client_patch.py

--- client/patch/nt_fovchanger_client_patch.py
+++ client/patch/nt_fovchanger_client_patch.py
@@ -1,72 +1,54 @@
 #!/usr/bin/env python3
 
-"""FOV patch for for the NT client binary with SourceMod plugin controllable adjustment support."""
+# Patch for the NT client binary (client.dll) to enable SourceMod plugin support for field-of-view control.
 
 import os
 import shutil
 import sys
 
+def create_file_backup(file_path):
+    backup_file = f"{file_path}.bak"
+    shutil.copy(file_path, backup_file)
+    print(f"Backup created: {backup_file}")
+    return backup_file
 
-def create_clientdll_backup(file_path: str) -> bool:
-    """Backs up the file_path as file_path.backup, if the backup doesn't exist yet."""
-    backup_file = f"{file_path}.backup"
-    if not os.path.exists(backup_file):
-        print(f'Backing up file "{file_path}" as "{backup_file}"')
-        shutil.copyfile(file_path, backup_file)
-        return True
-    print(f'Skipping writing of backup file "{backup_file}" since it already exists.')
-    return False
+def patch_file(file_path):
+    try:
+        # Create a backup of the original file
+        backup_path = create_file_backup(file_path)
 
+        # Define the patch data and offsets
+        patch_data = [
+            (b"\xD8\x8A\x3F", 0x2166B2),  # 3 bytes at offset 2166B2 (h)
+            (b"\x66\x6F\x76\x69\x73\x70\x61\x74\x63\x68\x65\x64", 0x30940C),  # 12 bytes at offset 30940C (h)
+        ]
 
-def patch_closecaption_to_fovispatched(file_path: str) -> None:
-    """Patch the first instance of b"closecaption" to b"fovispatched".
-    Expects to find exactly 5 instances of b"closecaption" bytes,
-    as is the case with a binary where this hasn't been modified.
-    """
-    with open(file_path, mode="rb+") as f:
-        data = f.read()
-        pattern = b"closecaption"
-        replace_pattern_with = b"fovispatched"
-        assert len(pattern) == len(replace_pattern_with)
+        # Read the binary file
+        with open(file_path, "rb+") as dll_file:
+            for data, offset in patch_data:
+                dll_file.seek(offset)
+                dll_file.write(data)
 
-        offset = 0
-        matches = []
-        while True:
-            try:
-                offset += data[offset:].index(pattern)
-                matches.append(offset)
-                offset += 1
-            except ValueError:
-                break
-        assert len(matches) == 5
-        # The target offset here is the first match.
-        closecaption_offset = matches[0]
+        print("Patching successful!")
 
-        f.seek(closecaption_offset)
-        f.write(replace_pattern_with)
-        print(f"Successfully patched byte(s) at offset: {closecaption_offset}")
-
-
-def patch_fov_entprop(file_path: str):
-    """Patch the FOV entprop of client."""
-    raise NotImplementedError  # FIXME
-
-
-def main() -> None:
-    """Entry point."""
-    if len(sys.argv) != 2:
-        print(
-            f'Usage: python "{sys.argv[0]}" '
-            r'"C:\path\to\Steam\steamapps\common\NEOTOKYO\NeotokyoSource\bin\client.dll"'
-        )
-        return
-    file_path = sys.argv[1]
-    assert os.path.exists(file_path), f"Path doesn't exist: {file_path}"
-    assert os.path.isfile(file_path), f"Path is not a file: {file_path}"
-    create_clientdll_backup(file_path)
-    patch_closecaption_to_fovispatched(file_path)
-    patch_fov_entprop(file_path)
-
+    except Exception as e:
+        print(f"Error: {e}")
+        if os.path.exists(backup_path):
+            print("Restoring backup...")
+            shutil.move(backup_path, file_path)
+            print("Backup restored.")
+        sys.exit(1)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python patch_dll.py <dll_file_path>")
+        sys.exit(1)
+
+    dll_file_path = sys.argv[1]
+    if not os.path.isfile(dll_file_path):
+        print("Error: The specified file does not exist.")
+        sys.exit(1)
+
+    # Patch the .dll
+    patch_file(dll_file_path)
+    
